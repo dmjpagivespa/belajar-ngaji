f9b6a601db862a26ae293f4f701842e3c252153f
SBC4 <SBC4@SBC13>
2015-04-23 13:20:07 +0700

    Cara menulis yang baik

---
+++ src/dmjpagivespa/Dmjpagivespa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dmjpagivespa;
+
+/**
+ *
+ * @author SBC4
+ */
+public class Dmjpagivespa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
